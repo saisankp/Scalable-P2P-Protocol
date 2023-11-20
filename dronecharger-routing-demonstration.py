54e1f8b6faf84a48422b882415754bc99e94696a
Sean Karl Dowling <dowlins6@local.scss.tcd.ie>
2023-11-20 19:02:38 +0000

    Removed some prints

--- dronecharger-routing-demonstration.py
+++ dronecharger-routing-demonstration.py
@@ -224,7 +224,6 @@ def send_requested_data(message, address):
     requested_data = decrypt(message, private_key).split('/')[3]
     # Package the data into a packet
     data_response = "data"+"/"+str(interest_code)+"/"+str(requested_device)+"/"+str(getattr(drone_charger, requested_data))
-    print("sent to" + str(address), str(getattr(drone_charger, requested_data)))
     device_socket.sendto(encrypt(data_response, knownPublicKeys[str(address)]), address)
 
 
